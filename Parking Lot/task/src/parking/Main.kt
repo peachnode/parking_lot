f1e9517f815e854e32d8b4e6697cd8ea7af2c8ba
araiwa <naomi.araiwa@googlemail.com>
2023-05-06 21:08:24 +0200

    Initial commit

---
+++ Parking Lot/task/src/parking/Main.kt
@@ -0,0 +1,11 @@
+package parking
+
+fun main() {
+    println(
+        """
+        White car has parked.
+        Yellow car left the parking lot.
+        Green car just parked here.
+        """.trimIndent()
+    )
+}
